8215ef4fcf56b350567b3f98a1c47cb0276f7dce
del-passero <ap.vorobyev@gmail.com>
2025-07-03 22:03:53 +0300

    test forApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { useEffect } from "react";
-
-export const tg = window.Telegram?.WebApp;
-
-export default function App() {
-  useEffect(() => {
-    tg?.ready();
-  }, []);
-
-  return (
-    <button
-      onClick={() => {
-        if (tg) {
-          tg.showAlert("Это Telegram WebApp!");
-        } else {
-          alert("Вы не в Telegram WebApp!!!");
-        }
-      }}
-    >
-      Проверить Telegram API
-    </button>
-  );
-}
+<button
+  onClick={() => {
+    console.log("tg = ", tg);
+    if (tg) {
+      tg.showAlert("Это Telegram WebApp!");
+    } else {
+      alert("Вы не в Telegram WebApp!!!");
+    }
+  }}
+>
+  Проверить Telegram API
+</button>
