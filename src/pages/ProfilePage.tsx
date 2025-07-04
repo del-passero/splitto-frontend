09e99e20016d08a02b8f091c6ca5f8af8dad7f8c
del-passero <ap.vorobyev@gmail.com>
2025-07-04 06:57:17 +0300

    первый тест аутентификации

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -13,7 +13,7 @@ export default function ProfilePage() {
     const tg = window.Telegram?.WebApp;
     const initData = tg?.initData;
     if (!initData) {
-      setError("Нет initData из Telegram ");
+      setError("Нет initData из Telegram WebApp, вот так вот!!!!");
       setLoading(false);
       return;
     }
