b0bb97b66e272f015aa1913da66dceba7b73e3c8
del-passero <ap.vorobyev@gmail.com>
2025-07-07 22:50:48 +0300

    начало оформления

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@
 import ReactDOM from "react-dom/client";
 import { ThemeLangProvider } from "./contexts/ThemeLangContext";
 import { UserProvider } from "./contexts/UserContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // Tailwind + кастомные стили
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeLangProvider>
     <UserProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </ThemeLangProvider>
 );
